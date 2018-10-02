88ff1632198cb8e1153c250441f1ccd07db46b92
brennanclark <brennanpclark@gmail.com>
2018-10-02 21:44:14 +0000

    Initial Commit

---
+++ express_server.js
@@ -0,0 +1,33 @@
+let express = require("express");
+let app = express();
+let PORT = 8080;
+
+app.set("view engine", "ejs");
+
+let urlDatabase = {
+  "b2xVn2": "http://wwww.lighthouselabs.ca",
+  "9sm5xK": "http://wwww.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
+})
+
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) =>{
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
