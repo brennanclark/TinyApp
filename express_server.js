3ce4fa2df85bd4eff12827e44f0fc8b55362771f
brennanclark <brennanpclark@gmail.com>
2018-10-03 01:38:04 +0000

    set up the /urls/new and adds the generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let app = express();
 let PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +11,16 @@ let urlDatabase = {
   "b2xVn2": "http://wwww.lighthouselabs.ca",
   "9sm5xK": "http://wwww.google.com"
 };
+
+function generateRandomString(){
+  return "123456".split('').map(function()
+    {return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.charAt
+    (Math.floor(62*Math.random()));}).join('');
+  //random link generator found online at stack overflow "https://stackoverflow.com/questions/1349404
+  ///generate-random-string-characters-in-javascript/26682781"
+
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,6 +31,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
@@ -26,7 +43,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
