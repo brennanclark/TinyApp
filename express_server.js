da16274d9debf99ac8a258bb83da810b36fe1572
brennanclark <brennanpclark@gmail.com>
2018-10-05 04:39:45 +0000

    URLS now belong to unique users. Need to implement privacy

--- express_server.js
+++ express_server.js
@@ -9,20 +9,26 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    "b2xVn2": {
+    userID: "userRandomID",
+    longURL: "http://www.lighthouselabs.ca"
+  },
+  "9sm5xK": {
+    userID: "userRandomID",
+    longURL: "http://www.google.com"
+  }
 };
 
-const users = {
+var users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-mokey-dinosaur"
+    password: "a"
   },
   "user2RandomID": {
-    id: "user2randomID",
+    id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: "a"
   }
 };
 
@@ -37,7 +43,6 @@ function generateRandomString(){
 function getUserByEmail (email) {
   for (let userID in users) {
     let user = users[userID];
-    console.log("one suer", user, email, user.email === email);
     let existingEmail = user.email;
     if (email === existingEmail){
       return user;
@@ -47,7 +52,8 @@ function getUserByEmail (email) {
 };
 
 
-//the root should eventually be the home page
+//---------------------------------HOME--------------------------------
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -60,6 +66,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//the PUSH comes in here and now we update the url database to have the user id appended to the end of the url
+
+app.post("/urls", (req, res) => {
+
+  let shortURL = generateRandomString();
+  let newlongURL = req.body.longURL;
+  let userID = req.body.id
+  console.log("shortURL", shortURL);
+
+  //this will have the userID as another key value pair
+  urlDatabase[shortURL] = {
+    userID: userID,
+    longURL: newlongURL
+  };
+  //res.redirect(`/urls/${shortURL}`);
+  res.redirect('/urls');
+
+});
+
+//---------------------------------NEW--------------------------------
+
+
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     user: users[req.cookies["user_id"]]
@@ -67,6 +95,11 @@ app.get("/urls/new", (req, res) => {
 
   res.render("urls_new",templateVars);
 });
+
+//When the user creates a new url it sends a post request to /urls
+
+//---------------------------------URLS/:Id--------------------------------
+
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
@@ -77,32 +110,20 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL
+  res.redirect("/urls");
+});
+
+//---------------------------------REGISTER--------------------------------
+
 app.get("/register", (req, res) => {
   res.render("registration_new");
 });
 
-app.get("/login", (req, res) =>{
-
-  res.render("reg_login");
-
-})
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-
-
-
-
-//adds a new object to the global users object
 app.post("/register", (req, res) => {
   let randomID = generateRandomString();
+
   if (req.body.email === '' || req.body.password === '') {
     res.status(400).send('Email or password empty');
     return
@@ -111,6 +132,7 @@ app.post("/register", (req, res) => {
     res.status(400).send('Email already exists');
     return
   }
+
   users[randomID] = {
     id: randomID,
     email: req.body.email,
@@ -121,21 +143,11 @@ app.post("/register", (req, res) => {
   console.log(users);
 });
 
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  let newlongURL = req.body.longURL;
-  urlDatabase[shortURL] = newlongURL;
-  res.redirect(`/urls/${shortURL}`);
-});
+//--------------------------------LOGIN-------------------------------
 
-app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect("/urls");
-});
 
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL
-  res.redirect("/urls");
+app.get("/login", (req, res) =>{
+  res.render("reg_login");
 });
 
 app.post("/login", (req, res) => {
@@ -155,19 +167,31 @@ app.post("/login", (req, res) => {
   }
 
   res.cookie('user_id', user.id);
-  res.redirect("/");
+  res.redirect("/urls/new");
 });
+
+//---------------------------------LOGOUT--------------------------------
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect("/urls");
+});
+
+//---------------------------------DELETE--------------------------------
 
 app.post("/urls/:id/delete", (req, res) =>{
   delete urlDatabase[req.params.id];
   res.redirect("/urls")
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
-
-
-
-
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 
 app.listen(PORT, () => {
